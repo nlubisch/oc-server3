ca5979d63490777632c59c7855679f02dd96beef
MacGyver-NRW <mirco@clanfamily.de>
2017-09-21 10:51:24 +0200

    Intercommit

--- htdocs/src/AppBundle/Controller/KitchenSinkController.php
+++ htdocs/src/AppBundle/Controller/KitchenSinkController.php
@@ -2,17 +2,62 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
+
+
+
+
+/**
+ * Class KitchenSinkController
+ *
+ * @package AppBundle\Controller
+ */
 class KitchenSinkController extends Controller
 {
     /**
-     * @Route("/kitchensink")
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @var string[]
      */
-    public function indexAction()
+    private $sidebarMainMenu = [
+        'home' => [
+            'href' => 'home',
+            'caption' =>'Übersicht'
+        ],
+        'style' =>[
+            'href' => 'style',
+            'caption' => 'Style'
+        ],
+        '404' => [
+            'href' => '404',
+            'caption' =>'Fehler']
+    ];
+
+    /**
+     * @Route("/kitchensink/{page}", defaults={"page" = "home"})
+     * @param string $page
+     *
+     * @return Response
+     */
+    public function indexAction($page)
     {
-        return $this->render('AppBundle:KitchenSinkController:index.html.twig');
+        if (!isset($this->sidebarMainMenu[$page])) {
+            $page = '404';
+        }
+
+        return $this->render(
+            '@App/KitchenSinkController/' . $page . '.html.twig',
+            [
+                'site' => [
+                    'title' => 'Opencaching Kitchensink',
+                    'page' => $page,
+                    'version' => '1.0.0',
+                    'base_url' => '/kitchensink',
+                    'navigation' => $this->sidebarMainMenu
+                ],
+            ]
+        );
     }
 }
+
